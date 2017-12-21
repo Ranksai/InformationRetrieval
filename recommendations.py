c23d42419270329a3989372558b3068644717538
Ranksai <ranks.j.inter1908@gmail.com>
2017-12-21 16:40:57 +0900

    Taro追加

--- recommendations.py
+++ recommendations.py
@@ -1,15 +1,16 @@
 from math import sqrt
 
-critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},
-'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 3.5},
-'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0, 'Superman Returns': 3.5, 'The Night Listener': 4.0},
-'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'The Night Listener': 4.5, 'Superman Returns': 4.0,
+critics={
+   'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},
+   'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 3.5},
+   'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0, 'Superman Returns': 3.5, 'The Night Listener': 4.0},
+   'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'The Night Listener': 4.5, 'Superman Returns': 4.0,
 'You, Me and Dupree': 2.5},
-'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
-'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 2.0},
-'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
-'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
-'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+   'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 2.0},
+   'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+   'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0},
+   'Taro':{'Lady in the Water': 4.0,'Snakes on a Plane': 3.5,'Just My Luck': 2.5, 'Superman Returns': 4.0}
+}
 
 # person1とperson2の距離を基にした類似性スコアを返す
 def sim_distance(prefs,person1,person2):
@@ -141,3 +142,20 @@ def getRecommendedItems(prefs,itemMatch,user):
    rankings.sort( )
    rankings.reverse( )
    return rankings
+
+
+if __name__ == '__main__':
+   print(critics['Taro']['Lady in the Water'])
+   matches = topMatches(critics, 'Taro', n=3)
+   print("near by Taro")
+   for i in range(len(matches)):
+      print(matches[i])
+   recomUserMatches = getRecommendations(critics, 'Taro')
+   print("recommend Taro")
+   for i in range(len(recomUserMatches)):
+      print(recomUserMatches[i])
+   itemsim=calculateSimilarItems(critics)
+   recomItemMatches = getRecommendedItems(critics,itemsim,'Toby')
+   print("recommend Taro")
+   for i in range(len(recomItemMatches)):
+      print(recomItemMatches[i])
